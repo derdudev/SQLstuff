587c3daa88a1308b822b5399b9da6804d4afd2c6
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2020-12-09 14:49:14 +0100

    Update lesson1.sql

--- lesson1.sql
+++ lesson1.sql
@@ -1,1 +1,24 @@
 SELECT SupplierName, City, Country FROM Suppliers WHERE Country LIKE "U%"
+
+SELECT * FROM Products WHERE Price < 50 AND Price > 20 --Gibt alle Produkte aus, deren Preis kleiner als 50 and größer als 20 ist.
+
+SELECT MAX(Price) FROM Products 
+
+SELECT MIN(Price) FROM Products
+
+SELECT AVG(Price) FROM Products
+
+SELECT SUM(Price) FROM Products
+
+SELECT * FROM Categories WHERE CategoryID = 2 OR CategoryID = 4 OR CategoryID = 6 --Wählt alle Kategorien mit Kategorie ID 2, 4 oder 6 aus. 
+
+SELECT COUNT (*) FROM Customers --Anzahl der Kunden
+
+SELECT COUNT (*) FROM Customers WHERE City = "London" --Anzahl der Kunden, die Sitz in London haben
+
+SELECT COUNT (*) FROM Customers WHERE City = "London" AND PostalCode = "WA1 1DP" OR PostalCode = "WX1 6LT"
+
+SELECT Price FROM Products ORDER BY Price ASC --Preise in aufsteigender Reihenfolge
+
+SELECT DISTINCT City From Customers --Wie viele Customer aus verschiedenen Städten?
+
